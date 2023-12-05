c8857b261d130513d9ca13c2a4e02f433dac0bf1
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-12-05 11:56:05 +0600

    added redux toolkit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,10 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
 import Payment from "./Payment/Payment";
 import PaymentHistory from "./PaymentHistory/PaymentHistory";
 
 const Home = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      <Payment />
+    <div className="flex justify-center ">
+      <div>
+        <button className="btn btn-outline">increment</button>
+        <p className="text-2xl">{count}</p>
+        <button className="btn btn-outline">decrement</button>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,18 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./component/Payment/Router.jsx";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.jsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <App />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
